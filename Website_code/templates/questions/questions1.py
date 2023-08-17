17f79c0b179423ca70664cdecddc98660faff83b
CrtAnton <ca.anton22@gmail.com>
2023-08-17 22:20:24 +0100

    added questions1.py

---
+++ Website_code/templates/questions/questions1.py
@@ -0,0 +1,192 @@
+questions = [
+    {
+        'question': 'Which approach involves analyzing support ticket data to identify common trends and areas for improvement?',
+        'options': ['Ticket Throwing', 'Support Ticket Deletion', 'Root Cause Analysis', 'Ticket Data Mining'],
+        'answer': 'Root Cause Analysis'
+    },
+    {
+        'question': 'In the context of applications support, what does "MTTR" stand for?',
+        'options': ['My Tires Turned Red', 'Mean Time to Raise', 'More Trouble Troubleshooting Required', 'Mean Time to Repair'],
+        'answer': 'Mean Time to Repair'
+    },
+    {
+        'question': 'Which practice involves testing new features or updates in a controlled environment before deploying them to production?',
+        'options': ['Random Feature Deployment', 'Feature Free-for-all', 'Feature Testing Grounds', 'Staging Environment'],
+        'answer': 'Staging Environment'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "ticket backlog"?',
+        'options': ['Collecting used tickets for recycling', 'Queue of unresolved support tickets', 'Creating a backlog of new tickets', 'Hiding tickets'],
+        'answer': 'Queue of unresolved support tickets'
+    },
+    {
+        'question': 'Which approach involves using automated scripts to replicate user actions and identify issues?',
+        'options': ['Manual Replication', 'Human Testing', 'Monkey Business', 'Automated Regression Testing'],
+        'answer': 'Automated Regression Testing'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "support knowledge base"?',
+        'options': ['Keeping secrets', 'Storing passwords', 'A central repository of support-related information to assist agents and users', 'Hiding support information'],
+        'answer': 'A central repository of support-related information to assist agents and users'
+    },
+    {
+        'question': 'Which metric assesses the overall satisfaction level of customers with the support they received?',
+        'options': ['Overall Joy Index', 'Customer Happiness Quotient', 'Customer Satisfaction Score', 'Support Happiness Factor'],
+        'answer': 'Customer Satisfaction Score'
+    },
+    {
+        'question': 'In the context of applications support, what does "RCA" stand for?',
+        'options': ['Royal Crown Affair', 'Root Cause Analysis', 'Recurring Customer Anguish', 'Reckless Change Adoption'],
+        'answer': 'Root Cause Analysis'
+    },
+    {
+        'question': 'Which practice involves automated testing of an application to ensure it continues to function as expected?',
+        'options': ['Automated Regression Testing', 'Manual Guesswork', 'Random Clicking', 'Automated Breakage Testing'],
+        'answer': 'Automated Regression Testing'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of "call deflection"?',
+        'options': ['Avoiding phone calls', 'Bouncing calls to different agents', 'Deflecting blame', 'Guiding users to self-service options'],
+        'answer': 'Guiding users to self-service options'
+    },
+    {
+        'question': 'Which approach involves customers helping each other by sharing their knowledge and experiences?',
+        'options': ['Support Standoff', 'Customer Arm Wrestling', 'Self-Service Support', 'Customer Community'],
+        'answer': 'Customer Community'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "support SLA"?',
+        'options': ['Stating Love Always', 'Supporting Slow Learning Agents', 'Defining the agreed-upon level of service provided to users', 'Support Super Large Agreement'],
+        'answer': 'Defining the agreed-upon level of service provided to users'
+    },
+    {
+        'question': 'Which metric measures the percentage of support tickets resolved within a single interaction?',
+        'options': ['First Query Resolution Rate', 'Initial Ticket Solution Index', 'First Response Success Rate', 'Single Interaction Closure Ratio'],
+        'answer': 'First Query Resolution Rate'
+    },
+    {
+        'question': 'In the context of support, what does the term "L1" refer to?',
+        'options': ['Level 1 Support', 'Level 1 Fun', 'Layer 1 of the support cake', 'Lumberjack 1'],
+        'answer': 'Level 1 Support'
+    },
+    {
+        'question': 'Which practice involves support teams working across different time zones to provide seamless coverage?',
+        'options': ['Moonlight Support', 'Follow-the-Sun Support', 'Daylight Avoidance Support', 'Time Zone Juggling'],
+        'answer': 'Follow-the-Sun Support'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "knowledge sharing sessions"?',
+        'options': ['Sharing top-secret recipes', 'Providing a platform for support agents to share experiences and insights', 'Exchanging movie recommendations', 'Hiding knowledge from others'],
+        'answer': 'Providing a platform for support agents to share experiences and insights'
+    },
+    {
+        'question': 'Which metric measures the average time it takes for a support agent to respond to a new ticket?',
+        'options': ['Initial Response Time Metric', 'First Ticket Reaction Rate', 'First Query Reaction Time', 'Ticket Arrival Speed Index'],
+        'answer': 'Initial Response Time Metric'
+    },
+    {
+        'question': 'In the context of support, what does the term "KPI" stand for?',
+        'options': ['Key Performance Indicator', 'Kung Fu Panda Impersonator', 'Keep People Informed', 'Knowledge Power Index'],
+        'answer': 'Key Performance Indicator'
+    },
+    {
+        'question': 'Which approach involves continuous monitoring of an application or service to detect issues in real-time?',
+        'options': ['Ignore and Pray', 'Proactive Monitoring', 'Reactive Monitoring', 'Sleeping on the Job'],
+        'answer': 'Proactive Monitoring'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "user self-service portals"?',
+        'options': ['Hiding user information', 'Preventing users from accessing support', 'Enabling users to find solutions on their own', 'Redirecting users to call centers'],
+        'answer': 'Enabling users to find solutions on their own'
+    },
+    {
+        'question': 'Which metric measures the time taken to acknowledge a new support ticket?',
+        'options': ['First Response Time Metric', 'Initial Ticket Greeting Index', 'First Query Greeting Time', 'Ticket Acknowledgment Speed Index'],
+        'answer': 'First Response Time Metric'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "support triage" process?',
+        'options': ['Solving puzzles', 'Sorting support agents by height', 'Prioritizing and categorizing support tickets for efficient handling', 'Running in circles'],
+        'answer': 'Prioritizing and categorizing support tickets for efficient handling'
+    },
+    {
+        'question': 'Which approach involves using historical data to forecast future support demands?',
+        'options': ['Predictive Analytics', 'Looking at the stars', 'Using a crystal ball', 'Random Guessing'],
+        'answer': 'Predictive Analytics'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "call routing"?',
+        'options': ['Sending calls to voicemail', 'Transferring calls to the CEO', 'Transferring calls to the right support agent based on skills and availability', 'Ignoring calls'],
+        'answer': 'Transferring calls to the right support agent based on skills and availability'
+    },
+    {
+        'question': 'Which metric measures the average time a support ticket remains unresolved?',
+        'options': ['Ticket Age Metric', 'Unresolved Ticket Duration Rate', 'Average Ticket Dormancy', 'Ticket Aging Index'],
+        'answer': 'Ticket Age Metric'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "support playbook"?',
+        'options': ['Creating theatrical performances', 'Documenting common support scenarios and their solutions for consistent handling', 'Organizing football strategies', 'Playing games'],
+        'answer': 'Documenting common support scenarios and their solutions for consistent handling'
+    },
+    {
+        'question': 'Which practice involves monitoring an application or service to identify and mitigate potential issues before they impact users?',
+        'options': ['Proactive Monitoring', 'Hiding in the Shadows', 'Blindfolding', 'Reactive Monitoring'],
+        'answer': 'Proactive Monitoring'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "support analytics"?',
+        'options': ['Predicting lottery numbers', 'Analyzing support-related data to gain insights and improve strategies', 'Analyzing customer\'s horoscopes', 'Analyzing music preferences'],
+        'answer': 'Analyzing support-related data to gain insights and improve strategies'
+    },
+    {
+        'question': 'Which metric measures the percentage of support tickets resolved during the first interaction?',
+        'options': ['First Contact Resolution Rate', 'Initial Ticket Success Index', 'First Query Closure Rate', 'Single Interaction Resolution Percentage'],
+        'answer': 'First Contact Resolution Rate'
+    },
+    {
+        'question': 'In the context of support, what does "SLC" stand for?',
+        'options': ['Support Level Check', 'Support Language Conversion', 'Service Level Commitment', 'Super Long Coffee'],
+        'answer': 'Service Level Commitment'
+    },
+    {
+        'question': 'Which approach involves using real-time chat for support agents to assist users?',
+        'options': ['Telegram Support', 'Telepathic Support', 'Live Chat Support', 'Snail Mail Support'],
+        'answer': 'Live Chat Support'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "support metrics"?',
+        'options': ['Decorating support tickets', 'Monitoring the length of support agent\'s fingernails', 'Measuring various aspects of support operations to assess performance and identify areas for improvement', 'Measuring the time it takes for a support agent to tie shoelaces'],
+        'answer': 'Measuring various aspects of support operations to assess performance and identify areas for improvement'
+    },
+    {
+        'question': 'Which practice involves using automated responses to acknowledge receipt of a support ticket?',
+        'options': ['Automated Acknowledgment', 'Auto-Reply Bot', 'Robotic Ticket Greeting', 'Automated Thank You'],
+        'answer': 'Auto-Reply Bot'
+    },
+    {
+        'question': 'In the context of support, what does the term "L2" refer to?',
+        'options': ['Level 2 Support', 'Level 2 Dance Moves', 'Layer 2 of the support cake', 'Llama 2'],
+        'answer': 'Level 2 Support'
+    },
+    {
+        'question': 'Which metric measures the percentage of support tickets resolved within the defined SLA timeframe?',
+        'options': ['SLA Compliance Rate', 'SLA Rebellion Index', 'SLA Ignorance Percentage', 'SLA Nonchalance Ratio'],
+        'answer': 'SLA Compliance Rate'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "support escalation"?',
+        'options': ['Escaping support duties', 'Prioritizing support tickets based on the level of anger from users', 'Raising the priority of a support ticket to a higher level of management for quicker resolution', 'Escalating support agents to superhero status'],
+        'answer': 'Raising the priority of a support ticket to a higher level of management for quicker resolution'
+    },
+    {
+        'question': 'Which approach involves monitoring and managing the performance and availability of applications?',
+        'options': ['Performance and Availability Management', 'Sleeping on the Job', 'Performance Management Only', 'Availability Management Only'],
+        'answer': 'Performance and Availability Management'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "support dashboard"?',
+        'options': ['Monitoring speed limits', 'Visualizing and presenting key support metrics and trends', 'Controlling a spaceship', 'Choosing a restaurant'],
+        'answer': 'Visualizing and presenting key support metrics and trends'
+    }
+]
