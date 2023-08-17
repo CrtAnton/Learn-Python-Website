ada3bf508c50b37d15d379d5a9f6153cb8d969fd
CrtAnton <ca.anton22@gmail.com>
2023-08-17 22:21:27 +0100

    added questions2.py

---
+++ Website_code/templates/questions/questions2.py
@@ -0,0 +1,192 @@
+questions = [
+    {
+        'question': 'What does the term "SLM" stand for in applications support?',
+        'options': ['Support Level Management', 'Service Lifecycle Monitoring', 'Service Level Management', 'System Log Maintenance'],
+        'answer': 'Service Level Management'
+    },
+    {
+        'question': 'Which approach involves a support team resolving issues by connecting to the user’s computer remotely?',
+        'options': ['Crowdsourcing', 'Remote Scripting', 'Self-service', 'On-site Support'],
+        'answer': 'Remote Scripting'
+    },
+    {
+        'question': 'Which factor is NOT considered when prioritizing support tickets?',
+        'options': ['Issue complexity', 'User seniority', 'Impact on business', 'Urgency'],
+        'answer': 'User seniority'
+    },
+    {
+        'question': 'What is the primary purpose of a "runbook" in applications support?',
+        'options': ['Decorative documentation', 'Cooking recipes', 'Automated deployment', 'Providing step-by-step guidance for issue resolution'],
+        'answer': 'Providing step-by-step guidance for issue resolution'
+    },
+    {
+        'question': 'What is the role of an "incident response plan" in applications support?',
+        'options': ['Managing payroll', 'Addressing long-term goals', 'Preparing for potential incidents and defining action steps', 'Creating new features'],
+        'answer': 'Preparing for potential incidents and defining action steps'
+    },
+    {
+        'question': 'Which metric measures the average time it takes to resolve a support ticket?',
+        'options': ['Resolution Rate', 'First Response Time', 'Service Availability', 'Mean Time to Resolution'],
+        'answer': 'Mean Time to Resolution'
+    },
+    {
+        'question': 'What does "root cause" refer to in the context of applications support?',
+        'options': ['The main customer', 'The underlying cause of an issue', 'A support team lead', 'The final resolution'],
+        'answer': 'The underlying cause of an issue'
+    },
+    {
+        'question': 'Which communication channel allows support teams to address multiple user queries simultaneously?',
+        'options': ['Fax', 'Smoke signals', 'Phone calls', 'Live Chat'],
+        'answer': 'Live Chat'
+    },
+    {
+        'question': 'In the context of support, what does "SLT" stand for?',
+        'options': ['Service Level Target', 'Service Level Tag', 'Superior Leadership Team', 'Support Lifecycle Time'],
+        'answer': 'Service Level Target'
+    },
+    {
+        'question': 'Which support practice involves categorizing and tagging support tickets for easy retrieval and analysis?',
+        'options': ['Ticket Sorting', 'Ticket Juggling', 'Ticket Tagging', 'Ticket Decorating'],
+        'answer': 'Ticket Tagging'
+    },
+    {
+        'question': 'What is the purpose of "release notes" in applications support?',
+        'options': ['Keeping track of employee attendance', 'Documenting software development methodologies', 'Providing information about new features, fixes, and known issues', 'Creating colorful diagrams'],
+        'answer': 'Providing information about new features, fixes, and known issues'
+    },
+    {
+        'question': 'What does "SLR" stand for in the context of applications support?',
+        'options': ['Service Level Request', 'Support Level Report', 'Service Level Requirement', 'Software Licensing Review'],
+        'answer': 'Service Level Requirement'
+    },
+    {
+        'question': 'Which metric assesses the percentage of support tickets resolved successfully on the first interaction?',
+        'options': ['First Contact Resolution Rate', 'First Ticket Resolution Index', 'First Response Success Rate', 'First Query Closure Ratio'],
+        'answer': 'First Contact Resolution Rate'
+    },
+    {
+        'question': 'What is the primary purpose of a "knowledge-centered support" approach?',
+        'options': ['Banning knowledge sharing', 'Prioritizing support requests', 'Creating knowledge silos', 'Building and sharing a collective support knowledge base'],
+        'answer': 'Building and sharing a collective support knowledge base'
+    },
+    {
+        'question': 'Which metric measures the percentage of time a service or application is available for use?',
+        'options': ['Uptime Percentage', 'Downtime Ratio', 'Resolution Success Rate', 'Latency Index'],
+        'answer': 'Uptime Percentage'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of "swarming"?',
+        'options': ['Studying insects', 'Assigning tasks randomly', 'Collaboratively working on a support ticket to quickly resolve issues', 'Creating chaos'],
+        'answer': 'Collaboratively working on a support ticket to quickly resolve issues'
+    },
+    {
+        'question': 'Which approach involves users resolving issues on their own using available resources?',
+        'options': ['Self-support', 'Peer pressure', 'Telepathy', 'Passing the buck'],
+        'answer': 'Self-support'
+    },
+    {
+        'question': 'What is the primary purpose of a "dashboard" in applications support?',
+        'options': ['Decorating the office', 'Monitoring and visualizing key support metrics', 'Driving on the highway', 'Changing tires'],
+        'answer': 'Monitoring and visualizing key support metrics'
+    },
+    {
+        'question': 'What does the term "severity" refer to in the context of support tickets?',
+        'options': ['The level of support agent expertise', 'How seriously the support agent takes the ticket', 'The impact and urgency of the issue', 'The number of times the issue occurred'],
+        'answer': 'The impact and urgency of the issue'
+    },
+    {
+        'question': 'Which metric measures the percentage of support requests that are resolved within the defined SLA timeframe?',
+        'options': ['SLA Compliance Rate', 'SLA Rebellion Index', 'SLA Ignorance Percentage', 'SLA Nonchalance Ratio'],
+        'answer': 'SLA Compliance Rate'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of "escalation"?',
+        'options': ['Climbing mountains', 'Transferring support agents to a different team', 'Taking a break', 'Raising the priority of a support ticket to ensure quicker resolution'],
+        'answer': 'Raising the priority of a support ticket to ensure quicker resolution'
+    },
+    {
+        'question': 'Which practice involves using historical data to identify trends, patterns, and insights in support operations?',
+        'options': ['Predictive Analytics', 'Reading tea leaves', 'Guessing', 'Ignoring data'],
+        'answer': 'Predictive Analytics'
+    },
+    {
+        'question': 'What does "NPS" stand for in the context of customer feedback?',
+        'options': ['Net Promoter Score', 'Never Provide Support', 'Nice Person Syndrome', 'New Product Showcase'],
+        'answer': 'Net Promoter Score'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "trend analysis"?',
+        'options': ['Predicting fashion trends', 'Ignoring data trends', 'Identifying recurring patterns and anomalies to enhance support strategies', 'Watching movies'],
+        'answer': 'Identifying recurring patterns and anomalies to enhance support strategies'
+    },
+    {
+        'question': 'Which approach involves a support team rotating shifts to provide 24/7 coverage?',
+        'options': ['Shiftless Support', 'Round-the-Clock Support', 'Supportless Rotation', 'Shiftful Neglect'],
+        'answer': 'Round-the-Clock Support'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of "churn rate"?',
+        'options': ['Measuring the speed of a blender', 'Calculating the rate at which customers stop using a service', 'Determining the number of support tickets closed within a day', 'Predicting customer preferences'],
+        'answer': 'Calculating the rate at which customers stop using a service'
+    },
+    {
+        'question': 'Which metric measures the percentage of customers who continue using a service after a specific period?',
+        'options': ['Customer Attrition Rate', 'Customer Seduction Rate', 'Customer Evaporation Index', 'Customer Retention Rate'],
+        'answer': 'Customer Retention Rate'
+    },
+    {
+        'question': 'In applications support, what does "CSAT" stand for?',
+        'options': ['Crazy Support Agent Team', 'Crying, Sighing, Annoyance, and Tantrums', 'Customer Satisfaction', 'Customer Support and Technology'],
+        'answer': 'Customer Satisfaction'
+    },
+    {
+        'question': 'Which practice involves categorizing and assigning support tickets based on specific skills and expertise?',
+        'options': ['Random Ticket Distribution', 'Ticket Throwing', 'Ticket Chucking', 'Skill-Based Routing'],
+        'answer': 'Skill-Based Routing'
+    },
+    {
+        'question': 'What is the purpose of "customer personas" in applications support?',
+        'options': ['Impersonating customers', 'Creating imaginary friends', 'Developing fictional profiles representing different customer types to tailor support strategies', 'Building a fan club'],
+        'answer': 'Developing fictional profiles representing different customer types to tailor support strategies'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "surge queue"?',
+        'options': ['Managing lines at amusement parks', 'Handling sudden influxes of support requests', 'Storing excess office supplies', 'Serving refreshments'],
+        'answer': 'Handling sudden influxes of support requests'
+    },
+    {
+        'question': 'Which metric assesses how quickly support agents respond to new tickets?',
+        'options': ['Response Time Metric', 'Speedy Gonzales Index', 'First Query Response Rate', 'Initial Ticket Reaction Ratio'],
+        'answer': 'Response Time Metric'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "reprioritization"?',
+        'options': ['Rearranging furniture', 'Changing support agent roles', 'Reevaluating and adjusting the priority of support tickets based on changing circumstances', 'Learning a new dance move'],
+        'answer': 'Reevaluating and adjusting the priority of support tickets based on changing circumstances'
+    },
+    {
+        'question': 'Which approach involves automating routine support tasks using AI or bots?',
+        'options': ['Bot-less Automation', 'Bot Rebellion', 'Artificial Intelligence Revolt', 'Chatbots and AI Automation'],
+        'answer': 'Chatbots and AI Automation'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "support SLA"?',
+        'options': ['Support Super Large Agreement', 'Stating Love Always', 'Supporting Slow Learning Agents', 'Defining the agreed-upon level of service provided to users'],
+        'answer': 'Defining the agreed-upon level of service provided to users'
+    },
+    {
+        'question': 'Which metric measures the percentage of customers who recommend a product or service to others?',
+        'options': ['Customer Referral Rate', 'Customer Complaint Ratio', 'Customer Silence Rate', 'Customer Grumbling Index'],
+        'answer': 'Customer Referral Rate'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "capacity management"?',
+        'options': ['Filling up storage rooms', 'Optimizing resource allocation to meet support demands', 'Managing cups and saucers', 'Calculating room temperature'],
+        'answer': 'Optimizing resource allocation to meet support demands'
+    },
+    {
+        'question': 'Which practice involves using a support portal or knowledge base for users to find solutions on their own?',
+        'options': ['Support Hide-and-Seek', 'Support Runaround', 'Self-Service Support', 'Support Tug-of-War'],
+        'answer': 'Self-Service Support'
+    }
+]
