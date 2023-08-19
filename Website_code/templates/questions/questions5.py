93a2a5353ccceecc62853484dfb3f37b921b3b28
CrtAnton <ca.anton22@gmail.com>
2023-08-19 19:30:58 +0100

    added questions5.py

---
+++ Website_code/templates/questions/questions5.py
@@ -0,0 +1,172 @@
+questions = [
+    {
+        'question': 'In applications support, what is the purpose of "vulnerability assessment"?',
+        'options': ['Assessing the vulnerability of support agents', 'Identifying security weaknesses in applications and systems', 'Assessing the vulnerability of office plants', 'Assessing the vulnerability of company picnics'],
+        'answer': 'Identifying security weaknesses in applications and systems'
+    },
+    {
+        'question': 'Which practice involves integrating development and IT operations to achieve faster and more reliable software delivery?',
+        'options': ['Lone Wolf Approach', 'Waterfall Methodology', 'Agile Transformation', 'DevOps'],
+        'answer': 'DevOps'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "root cause analysis"?',
+        'options': ['Digging for roots', 'Identifying the underlying cause of incidents or issues', 'Growing plants', 'Creating new issues'],
+        'answer': 'Identifying the underlying cause of incidents or issues'
+    },
+    {
+        'question': 'Which approach involves managing and prioritizing tasks based on their business value?',
+        'options': ['FIFO (First-In, First-Out)', 'LIFO (Last-In, First-Out)', 'Value-based Prioritization', 'Alphabetical Ordering'],
+        'answer': 'Value-based Prioritization'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "security incident response plan"?',
+        'options': ['Responding to the security of social events', 'Defining steps to address security breaches and incidents', 'Responding to the security of virtual reality', 'Creating security incidents for fun'],
+        'answer': 'Defining steps to address security breaches and incidents'
+    },
+    {
+        'question': 'Which practice involves continuous iteration and improvement of processes and products?',
+        'options': ['Stagnation Strategy', 'Set-and-Forget Approach', 'Agile Methodology', 'Static Workflow'],
+        'answer': 'Agile Methodology'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "communication plan"?',
+        'options': ['Planning daily communication meetings', 'Defining how and when to communicate with stakeholders', 'Planning a secret surprise party', 'Planning a communication-free day'],
+        'answer': 'Defining how and when to communicate with stakeholders'
+    },
+    {
+        'question': 'Which approach involves addressing user feedback and adapting software accordingly?',
+        'options': ['Ignore User Feedback', 'User-Centric Development', 'Feedback Elimination Strategy', 'Disregard User Needs'],
+        'answer': 'User-Centric Development'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "change management"?',
+        'options': ['Resisting change at all costs', 'Managing pocket change', 'Managing and controlling changes to applications or systems', 'Managing fashion trends'],
+        'answer': 'Managing and controlling changes to applications or systems'
+    },
+    {
+        'question': 'Which practice involves automating the deployment process to ensure consistency and reliability?',
+        'options': ['Manual Deployment', 'Chaotic Deployment', 'Automated Deployment', 'Random Deployment'],
+        'answer': 'Automated Deployment'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "stakeholder matrix"?',
+        'options': ['Creating a matrix for math enthusiasts', 'Identifying and analyzing stakeholders and their interests', 'Creating a matrix for superheroes', 'Creating a matrix for board games'],
+        'answer': 'Identifying and analyzing stakeholders and their interests'
+    },
+    {
+        'question': 'Which approach involves developing software in short cycles with frequent collaboration and feedback?',
+        'options': ['Long Cycle Development', 'Waterfall Methodology', 'Agile Development', 'Marathon Development'],
+        'answer': 'Agile Development'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "penetration testing"?',
+        'options': ['Testing pens for writing quality', 'Testing the durability of pens', 'Testing applications for security vulnerabilities by simulating attacks', 'Testing pens for artistic capabilities'],
+        'answer': 'Testing applications for security vulnerabilities by simulating attacks'
+    },
+    {
+        'question': 'Which practice involves cross-functional teams working together to develop and deliver software?',
+        'options': ['Solo Development', 'Individual Silos', 'Cross-Functional Collaboration', 'Separate Department Development'],
+        'answer': 'Cross-Functional Collaboration'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "incident communication"?',
+        'options': ['Communicating incidents through interpretive dance', 'Communicating incidents to a small group of friends', 'Effectively communicating incidents to stakeholders and users', 'Communicating incidents to distant galaxies'],
+        'answer': 'Effectively communicating incidents to stakeholders and users'
+    },
+    {
+        'question': 'Which practice involves automating the testing of software changes to ensure they don’t break existing functionality?',
+        'options': ['Manual Testing', 'Automated Testing', 'Random Testing', 'Boring Testing'],
+        'answer': 'Automated Testing'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of "user acceptance testing"?',
+        'options': ['Testing users for their acceptance of support agents', 'Testing the acceptance of new features or changes by users before deployment', 'Testing user tolerance levels', 'Testing users for their love of tacos'],
+        'answer': 'Testing the acceptance of new features or changes by users before deployment'
+    },
+    {
+        'question': 'Which approach involves creating a culture of collaboration between development and operations teams?',
+        'options': ['Silo Culture', 'Culture of Blame', 'DevOps Culture', 'Isolation Culture'],
+        'answer': 'DevOps Culture'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "risk assessment"?',
+        'options': ['Assessing the risk of going on vacation', 'Identifying and evaluating potential risks to applications and systems', 'Assessing the risk of eating ice cream', 'Assessing the risk of trying a new hairstyle'],
+        'answer': 'Identifying and evaluating potential risks to applications and systems'
+    },
+    {
+        'question': 'Which practice involves continuous integration and delivery of software changes to ensure stability and fast delivery?',
+        'options': ['Static Integration and Delivery', 'Occasional Integration and Delivery', 'Continuous Integration and Continuous Delivery (CI/CD)', 'Disconnected Integration and Delivery'],
+        'answer': 'Continuous Integration and Continuous Delivery (CI/CD)'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "stakeholder engagement plan"?',
+        'options': ['Engaging stakeholders in a dance-off', 'Planning how to engage and involve stakeholders throughout a project', 'Planning a stakeholder party', 'Planning a stakeholder hibernation plan'],
+        'answer': 'Planning how to engage and involve stakeholders throughout a project'
+    },
+    {
+        'question': 'Which approach involves addressing both functional and non-functional requirements in software development?',
+        'options': ['Functional Requirements Focus', 'One-sided Requirements Approach', 'Holistic Requirements Approach', 'Functional-only Requirements Approach'],
+        'answer': 'Holistic Requirements Approach'
+    },
+    {
+        'question': 'In applications support, what is the purpose of "continuous monitoring"?',
+        'options': ['Monitoring occasionally', 'Monitoring only on Mondays', 'Continuous tracking and observation of applications to ensure performance and security', 'Monitoring every leap year'],
+        'answer': 'Continuous tracking and observation of applications to ensure performance and security'
+    },
+    {
+        'question': 'Which practice involves addressing security throughout the software development lifecycle?',
+        'options': ['Security as an Afterthought', 'Secure Development Lifecycle (SDL)', 'Security as a Bonus', 'Security as a Side Project'],
+        'answer': 'Secure Development Lifecycle (SDL)'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "stakeholder communication plan"?',
+        'options': ['Planning daily stakeholder communication', 'Planning how to communicate with stakeholders to keep them informed and engaged', 'Planning a stakeholder silent retreat', 'Planning a stakeholder disguise plan'],
+        'answer': 'Planning how to communicate with stakeholders to keep them informed and engaged'
+    },
+    {
+        'question': 'Which approach involves involving stakeholders early and frequently throughout a project?',
+        'options': ['Stakeholder Neglect Approach', 'Stakeholder Involvement Approach', 'Stakeholder Isolation Approach', 'Stakeholder Avoidance Approach'],
+        'answer': 'Stakeholder Involvement Approach'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "security audit"?',
+        'options': ['Auditing security guards', 'Auditing the security of online accounts', 'Systematically evaluating the security measures of applications and systems', 'Auditing the security of recipes'],
+        'answer': 'Systematically evaluating the security measures of applications and systems'
+    },
+    {
+        'question': 'Which practice involves delivering small, incremental changes to software frequently?',
+        'options': ['Big Bang Deployment', 'Incremental Deployment', 'One-time Deployment', 'Huge Release Deployment'],
+        'answer': 'Incremental Deployment'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "stakeholder influence assessment"?',
+        'options': ['Assessing stakeholder influence in elections', 'Assessing and analyzing the influence and impact of stakeholders on a project', 'Assessing influence of random people', 'Assessing influence of pets'],
+        'answer': 'Assessing and analyzing the influence and impact of stakeholders on a project'
+    },
+    {
+        'question': 'Which practice involves planning and executing work in small, time-boxed iterations?',
+        'options': ['Long-term Planning', 'Sprint Planning', 'Marathon Planning', 'No Planning'],
+        'answer': 'Sprint Planning'
+    },
+    {
+        'question': 'In applications support, what is the purpose of a "security incident report"?',
+        'options': ['Reporting incidents involving security guards', 'Reporting incidents involving online accounts', 'Documenting and analyzing security incidents to prevent future occurrences', 'Reporting incidents involving food'],
+        'answer': 'Documenting and analyzing security incidents to prevent future occurrences'
+    },
+    {
+        'question': 'Which practice involves collaboration between development and operations teams throughout the software lifecycle?',
+        'options': ['Isolation of Development and Operations', 'DevOps Collaboration', 'Development-Only Approach', 'Operations-Only Approach'],
+        'answer': 'DevOps Collaboration'
+    },
+    {
+        'question': 'In the context of support, what is the purpose of a "stakeholder mapping"?',
+        'options': ['Creating a map for treasure hunting', 'Identifying and visualizing stakeholders and their relationships', 'Creating a map for space travel', 'Creating a map for underwater exploration'],
+        'answer': 'Identifying and visualizing stakeholders and their relationships'
+    },
+    {
+        'question': 'Which approach involves releasing small, frequent updates to applications to minimize risk and improve reliability?',
+        'options': ['Big Bang Release', 'Frequent Release', 'Infrequent Release', 'No Release'],
+        'answer': 'Frequent Release'
+    }
+]
